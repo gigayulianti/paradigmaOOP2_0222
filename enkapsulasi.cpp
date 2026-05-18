b8d6229db5ad11b863ecf2a6bd126b6ec2248b32
gigayulianti <yulianti2@gmail.com>
2026-05-18 18:46:13 +0700

    Commit 7

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -9,6 +9,20 @@ public:
         saklarNo[i] = value;
     }
     string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+
+
 
 
 
