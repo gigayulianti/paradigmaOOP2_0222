17361940a0bc8ead3eadb2d137226a5a562a5813
gigayulianti <yulianti2@gmail.com>
2026-05-18 18:11:06 +0700

    Commit 3

--- Polimorpi.cpp
+++ Polimorpi.cpp
@@ -20,3 +20,19 @@ class lia :public seseorang {
 public:
         void pesan(){
             cout << "pesan dari lia" << endl;
+                    }
+};
+
+int main() {
+    seseora* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    // a.seseorang: :pesan();
+    // b..seseorang: :pesan();
+    return 0;
+}
